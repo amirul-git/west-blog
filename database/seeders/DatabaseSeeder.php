3c1c3e516fc2e66e536533de004103a8d73bb77a
amir-repository <achmadnassardhi@gmail.com>
2023-07-27 00:16:01 +0700

    Add post and category seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Category;
+use App\Models\Post;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,11 +15,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
-
         \App\Models\User::factory()->create([
             'name' => 'Amir',
             'email' => 'amir@mail.com',
         ]);
+
+        Post::factory(10)->create();
+
+        Category::factory(2)->create();
     }
 }
