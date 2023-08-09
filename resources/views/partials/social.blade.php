a6d1304261d7bb439787af3c05560908a8c13358
amir-repository <achmadnassardhi@gmail.com>
2023-08-09 22:55:23 +0700

    Add correct link

--- resources/views/partials/social.blade.php
+++ resources/views/partials/social.blade.php
@@ -1,4 +1,5 @@
 <div class="text-sm">
-    <a href="#" class="px-2 py-1 bg-blue-600 rounded-md capitalize">github</a>
-    <a href="#" class="px-2 py-1 bg-blue-600 rounded-md capitalize">linkedin</a>
+    <a href="https://github.com/amirul-git" class="px-2 py-1 bg-blue-600 rounded-md capitalize">github</a>
+    <a href="https://www.linkedin.com/in/amirullah-achmad-nassardhi-7155a9195/"
+        class="px-2 py-1 bg-blue-600 rounded-md capitalize">linkedin</a>
 </div>
